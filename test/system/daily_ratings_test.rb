85c6a824840e4fdcbeeb75898d096943f42b58e6
Sophie Gill <sophie-gill@hotmail.com>
2020-05-10 16:50:07 +0100

    :white_check_mark: adds feature tests for adding a daily rating

---
+++ test/system/daily_ratings_test.rb
@@ -0,0 +1,19 @@
+require 'application_system_test_case'
+
+class DailyRatingsTest < ApplicationSystemTestCase
+  test 'visiting the index' do
+    visit root_url
+
+    assert_selector 'h1', text: 'Happy App'
+  end
+
+  test 'creating a daily raiting' do
+    visit root_url
+    click_link 'Rate my day 📝'
+    choose('daily_rating_rating_6')
+    click_button('Save Daily rating')
+
+    assert_selector 'h1', text: 'Happy App'
+    assert_current_path '/'
+  end
+end
